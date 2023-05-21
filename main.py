c4c953dfca384d7c43021070c78fd92eee568466
aalexeei <kai2001@ukr.net>
2023-05-21 09:21:58 +0300

    add test for funct parse_cookie

--- main.py
+++ main.py
@@ -25,3 +25,12 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;country=Russia;') == {'name': 'Dima', 'age': '28', 'country': 'Russia'}
+    assert parse_cookie('name=Dima=;age=28;') == {'name': 'Dima=', 'age': '28'}
+    assert parse_cookie('name=Dima;age=;') == {'name': 'Dima', 'age': ''}
+    assert parse_cookie('name=Dima;age=28;name=Vasya') == {'name': 'Vasya', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;name=Dima') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima;age=28;age=30') == {'name': 'Dima', 'age': '30'}
+    assert parse_cookie('=Dima;age=28;') == {'': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima;=28;') == {'name': 'Dima', '':
