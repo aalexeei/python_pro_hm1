f4bbd02a4598f4c321700465ffcd09c015269c7d
aalexeei <kai2001@ukr.net>
2023-05-21 09:08:40 +0300

    update funct parse_cookie and parse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def parse(query: str) -> dict:
-    return {}
+from urllib.parse import urlparse, parse_qs
 
+def parse(query: str) -> dict:
+    params = urlparse(query).query
+    return dict(parse_qs(params))
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
@@ -9,13 +11,18 @@ if __name__ == '__main__':
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
 
-
 def parse_cookie(query: str) -> dict:
-    return {}
-
+    cookies = query.split(';')
+    result = {}
+    for cookie in cookies:
+        if cookie:
+            name, value = cookie.split('=')
+            result[name.strip()] = value.strip()
+    return result
 
 if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
