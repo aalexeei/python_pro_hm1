1fab167e6273fce740141c112df853fe9a2c6cd1
aalexeei <kai2001@ukr.net>
2023-05-21 09:14:29 +0300

    update tests  parse_cookie

--- main.py
+++ main.py
@@ -21,8 +21,7 @@ def parse_cookie(query: str) -> dict:
     return result
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age'
